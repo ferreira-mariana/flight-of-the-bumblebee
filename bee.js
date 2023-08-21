90045d3eb18407739a287d71791c4ce852ea991e
Mariana Ferreira <ferreira_mariana@id.uff.br>
2023-08-20 21:43:13 -0300

    feat: create game

---
+++ bee.js
@@ -0,0 +1,39 @@
+class Bee {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = 60;
+  }
+
+  drawBee(x, y) {
+    this.x = x;
+    this.y = y;
+
+    push();
+    fill(255, 255, 204);
+    stroke(185, 185, 134);
+    ellipse(x - 15, y + 8, 15, 10); // left bottom wing
+    ellipse(x + 15, y + 8, 15, 10); // right bottom wing
+    ellipse(x - 15, y - 2, 25, 20); // left top wing
+    ellipse(x + 15, y - 2, 25, 20); // right top wing
+
+    noStroke();
+    fill(250, 205, 70);
+    ellipse(x, y, 24, 34); // bee's body
+    fill(70);
+    rect(x - 13, y - 5, 26, 8, 6);
+    arc(x, y + 12, 16, 16, 0, PI, OPEN);
+
+    stroke(70);
+    line(x - 5, y - 12, x - 10, y - 20); // right antenna
+    ellipse(x - 10, y - 20, 3);
+    line(x + 5, y - 12, x + 10, y - 20); // left antenna
+    ellipse(x + 10, y - 20, 3);
+    pop();
+  }
+
+  clicked(px, py) {
+    const d = dist(px, py, this.x, this.y);
+    return d <= this.radius;
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+const maxLevel = 10;
+const states = {
+  initial: 0,
+  playing: 1,
+  end: 2,
+};
+
+let bee;
+let song;
 let xoff1 = 0;
 let xoff2 = 10000;
-let started = false;
-let song;
+let state = states.initial;
+let lvl = 1;
 
 function preload() {
   soundFormats("mp3");
@@ -10,63 +19,96 @@ function preload() {
 
 function setup() {
   const cnv = createCanvas(windowWidth, windowHeight);
-  cnv.mousePressed(controlFlow);
+  cnv.mousePressed(startGame);
   textSize(24);
+  bee = new Bee(windowWidth/2, windowHeight/2);
 }
 
 function draw() {
   background(110, 180, 225);
-  if (started) {
+
+  if (lvl > maxLevel) {
+    return endGame();
+  }
+
+  if (state === states.playing) {
     const x = map(noise(xoff1), 0, 1, 0, width);
     const y = map(noise(xoff2), 0, 1, 0, height);
 
-    xoff1 += 0.02;
-    xoff2 += 0.02;
+    xoff1 += 0.002 * lvl;
+    xoff2 += 0.002 * lvl;
 
-    drawBee(x, y);
-  } else {
+    bee.drawBee(x, y);
+
+    push();
     fill(56, 111, 148);
     noStroke();
-    text("click or press enter to play", 30, 50);
+    textSize(20);
+    text(
+      `level ${lvl} - click on the bee to level up`,
+      30,
+      30,
+      width - 60,
+      height - 60
+    );
+    textSize(14);
+    text(`press esq to stop`, 30, height - 30, width - 60, height - 60);
+    pop();
+  } else if (state === states.end) {
+    push();
+    fill(56, 111, 148);
+    noStroke();
+    textSize(60);
+    textAlign(CENTER, CENTER);
+    text("YOU WON!", width / 2, height / 2);
+    textSize(16);
+    text(`click or press enter to play again`, width / 2, height - 30);
+    pop();
+  } else {
+    push();
+    fill(56, 111, 148);
+    noStroke();
+    text("click or press enter to play", 30, 30, width - 60, height - 60);
     drawPlayButton();
+    pop();
   }
 }
 
-function controlFlow() {
-  started = !started;
-  if (started) {
+function startGame() {
+  if (state !== state.playing && !song.isPlaying()) {
+    lvl = 1;
+    state = states.playing;
+    song.setLoop(true);
     song.play();
-  } else {
-    song.stop();
+  }
+}
+
+function endGame() {
+  song.stop();
+  state = states.end;
+  lvl = 1;
+}
+
+function resetGame() {
+  song.stop();
+  state = states.initial;
+}
+
+function mouseClicked() {
+  if (state === states.playing) {
+    if (bee.clicked(mouseX, mouseY)) {
+      lvl++;
+    }
   }
 }
 
 function keyPressed() {
   if (keyCode === ENTER) {
-    controlFlow();
+    startGame();
   }
-}
-
-function drawBee(x, y) {
-  fill(255, 255, 204);
-  stroke(185, 185, 134);
-  ellipse(x - 15, y + 8, 15, 10); // left bottom wing
-  ellipse(x + 15, y + 8, 15, 10); // right bottom wing
-  ellipse(x - 15, y - 2, 25, 20); // left top wing
-  ellipse(x + 15, y - 2, 25, 20); // right top wing
-
-  noStroke();
-  fill(250, 205, 70);
-  ellipse(x, y, 24, 34); // bee's body
-  fill(70);
-  rect(x - 13, y - 5, 26, 8, 6);
-  arc(x, y + 12, 16, 16, 0, PI, OPEN);
-
-  stroke(70);
-  line(x - 5, y - 12, x - 10, y - 20); // right antenna
-  ellipse(x - 10, y - 20, 3);
-  line(x + 5, y - 12, x + 10, y - 20); // left antenna
-  ellipse(x + 10, y - 20, 3);
+  if (keyCode === ESCAPE) {
+    resetGame();
+  }
 }
 
 function drawPlayButton() {
